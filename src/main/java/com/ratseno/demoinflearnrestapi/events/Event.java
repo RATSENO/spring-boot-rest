9b307a8f85aae66375efc8a78e354c16756fb624
RATSENO <onestar1014@gmail.com>
2018-12-17 22:33:24 +0900

    [ADD]Event Domain 생성

---
+++ src/main/java/com/ratseno/demoinflearnrestapi/events/Event.java
@@ -0,0 +1,31 @@
+package com.ratseno.demoinflearnrestapi.events;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Builder @AllArgsConstructor @NoArgsConstructor
+@Getter @Setter @EqualsAndHashCode(of = "id")
+public class Event {
+	
+	private Integer id;
+	private String name;
+	private String description;
+	private LocalDateTime beginEnrollmentDateTime;
+	private LocalDateTime closeEnrollmentDateTime;
+	private LocalDateTime beginEventDateTime;
+	private LocalDateTime endEventDateTime;
+	private String loaction;	// optional
+	private int basePrice; 		// optional
+	private int maxPrice;		//optional
+	private int limitOfEnrollment;	//optional
+	private boolean offline;
+	private boolean free;
+	private EventStatus eventStatus; 
+
+}
---
+++ src/main/java/com/ratseno/demoinflearnrestapi/events/EventStatus.java
@@ -0,0 +1,5 @@
+package com.ratseno.demoinflearnrestapi.events;
+
+public enum EventStatus {
+	DRAFT, PUBLISHED, BEGAN_ENROLLMENT; 
+}
