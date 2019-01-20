7dddb6cda9bb17d582be9bcdcf1674b52424f7ea
RATSENO <onestar1014@gmail.com>
2019-01-20 14:57:58 +0900

    [MOD]JUnit 테스트 설명용 어노테이션 생성

--- src/test/java/com/ratseno/demoinflearnrestapi/events/EventControllerTests.java
+++ src/test/java/com/ratseno/demoinflearnrestapi/events/EventControllerTests.java
@@ -21,6 +21,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ratseno.demoinflearnrestapi.common.TestDescription;
 
 /**
  * @author onestar
@@ -43,6 +44,7 @@ public class EventControllerTests {
 	ObjectMapper objectMapper; 
 	
 	@Test
+	@TestDescription("정상적으로 이벤트를 생성하는 테스트")
 	public void createEvent() throws Exception {
 		EventDto event = EventDto.builder()
 				.name("Spring")
@@ -73,6 +75,7 @@ public class EventControllerTests {
 	
 
 	@Test
+	@TestDescription("입력 받을 수 없는 값을 사용한 경우에 에러가 발생하는 테스트")
 	public void createEvent_Bad_Request() throws Exception {
 		Event event = Event.builder()
 				.id(100)
@@ -101,6 +104,7 @@ public class EventControllerTests {
 	
 	
 	@Test
+	@TestDescription("입력 값이 비어있는 경우에 에러가 발생하는 테스트")
 	public void createEvent_Bad_Request_Empty_Input() throws Exception {
 		EventDto eventDto = EventDto.builder().build();
 		
@@ -112,6 +116,7 @@ public class EventControllerTests {
 	}
 
 	@Test
+	@TestDescription("입력 값이 잘못된 경우에 에러가 발생하는 테스트")
 	public void createEvent_Bad_Request_Wrong_Input() throws Exception {
 		EventDto eventDto = EventDto.builder()
 				.name("Spring")
