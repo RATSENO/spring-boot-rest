b1f46438f40ceb5a62ab72707e497ce835fc8cf6
RATSENO <onestar1014@gmail.com>
2019-01-20 14:00:25 +0900

    [MOD]입력값 이외에 에러발생
    어플리케이션 프로퍼티
    
    spring.jackson.deserialization.fail-on-unknown-properties=true
    추가
    jackson의 JSON에서 객체로 변환시 없는 프로퍼티때 에러(deserialization)

--- src/test/java/com/ratseno/demoinflearnrestapi/events/EventControllerTests.java
+++ src/test/java/com/ratseno/demoinflearnrestapi/events/EventControllerTests.java
@@ -40,6 +40,36 @@ public class EventControllerTests {
 	
 	@Test
 	public void createEvent() throws Exception {
+		EventDto event = EventDto.builder()
+				.name("Spring")
+				.description("REST API development with Spring")
+				.beginEnrollmentDateTime(LocalDateTime.of(2018, 11, 23, 14, 21))
+				.closeEnrollmentDateTime(LocalDateTime.of(2018, 11, 24, 14, 21))
+				.beginEventDateTime(LocalDateTime.of(2018, 11, 25, 14, 21))
+				.endEventDateTime(LocalDateTime.of(2018, 11, 26, 14, 21))
+				.basePrice(100)
+				.maxPrice(200)
+				.limitOfEnrollment(100)
+				.loaction("강남역 D2 스타트업 팩토리")
+				.build();
+		
+		mockMvc.perform(post("/api/events")
+					.contentType(MediaType.APPLICATION_JSON_UTF8)
+					.accept(MediaTypes.HAL_JSON)
+					.content(objectMapper.writeValueAsString(event)))
+				.andDo(print())
+				.andExpect(status().isCreated())
+				.andExpect(jsonPath("id").exists())
+				.andExpect(header().exists(HttpHeaders.LOCATION))
+				.andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_UTF8_VALUE))
+				.andExpect(jsonPath("id").value(Matchers.not(100)))
+				.andExpect(jsonPath("free").value(Matchers.not(true)))
+				.andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name()));
+	}
+	
+
+	@Test
+	public void createEvent_Bad_Request() throws Exception {
 		Event event = Event.builder()
 				.id(100)
 				.name("Spring")
@@ -62,14 +92,7 @@ public class EventControllerTests {
 					.accept(MediaTypes.HAL_JSON)
 					.content(objectMapper.writeValueAsString(event)))
 				.andDo(print())
-				.andExpect(status().isCreated())
-				.andExpect(jsonPath("id").exists())
-				.andExpect(header().exists(HttpHeaders.LOCATION))
-				.andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_UTF8_VALUE))
-				.andExpect(jsonPath("id").value(Matchers.not(100)))
-				.andExpect(jsonPath("free").value(Matchers.not(true)))
-				.andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name()));
+				.andExpect(status().isBadRequest());
 	}
-	
-	
+
 }
